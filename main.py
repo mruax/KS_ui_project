c57f6774709cc45dc696fbb79636d4203a664238
mruax <matvey.chernyakov@yandex.ru>
2024-05-14 17:53:13 +0500

    Small changes

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from tkinter import *
+from tkinter.ttk import Frame
 from pathlib import Path
 
 from settings import *
@@ -6,28 +7,37 @@ from settings import *
 
 # Функции для изменения вида кнопки
 def on_start(e):
-    Button_start['image'] = b_start_light
-    Button_start['fg'] = fg_b
+    Button_start['image'] = b_start_dark
+    # Button_start['fg'] = fg_b
 
 
 def off_start(e):
     Button_start['image'] = b_start
-    Button_start['fg'] = fg_w
+    # Button_start['fg'] = fg_w
 
 
 def change_color_start(e):
-    Button_start['image'] = b_start
+    Button_start['image'] = b_start_light
 
 
-# Функция для очистки стартового окна, переход к окну 2
 def clearing_w_start():
-    # Удаление всех виджетов из окна
+    """
+    Удаление всех виджетов из окна.
+
+    :return: None
+    """
     for widget in W.winfo_children():
         widget.destroy()
 
 
 def w_2(e):
-    # Параметры для окна 2
+    """
+    Второе окно.
+
+    :param e:
+    :return: None
+    """
+
     W.title("БРОНИРОВАНИЕ")
     image_2 = PhotoImage(file='bg_2.png')
     bg_2 = Label(W, image=image_2, borderwidth=0)
@@ -41,16 +51,17 @@ if __name__ == "__main__":
     W.title("ХМЕЛИ СУНЕЛИ")
     W.resizable(False, False)
 
-    image_start = PhotoImage(file=Path(bg_with_logo))
+    image_start = PhotoImage(file=Path(bg_with_logo))  # Стартовый фон
+
+    # Изображения для разных фаз кнопки
+    b_start = PhotoImage(file=Path(button_start))
+    b_start_dark = PhotoImage(file=Path(button_start_dark))
+    b_start_light = PhotoImage(file=Path(button_start_light))
+
     bg_start = Label(W, image=image_start, borderwidth=0)
     bg_start.place(x=0, y=0)
 
-    # Кнопка на стартовом окне
-    # Изображения для разных фаз кнопки
-    b_start = PhotoImage(file=Path(button_start))
-    b_start_light = PhotoImage(file=Path(button_start_light))
-
-    # Создание кнопки (параметры)
+    # Создание кнопки с параметрами
     Button_start = Button(W, image=b_start, borderwidth=0,
                           text="ЗАБРОНИРОВАТЬ СТОЛ", compound="center", fg=fg_w, font=font1,
                           command=clearing_w_start)
